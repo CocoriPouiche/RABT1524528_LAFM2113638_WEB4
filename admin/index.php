ffc0f992d82cb939d59c92e80a6027099bc4ce19
Mikael G. Laferrière <2113638@cstj.qc.ca>
2025-04-07 09:36:06 -0400

    Ajout du css de base pour les reservations et les statistiques dans l'index admin

--- admin/index.php
+++ admin/index.php
@@ -61,6 +61,17 @@
         <p class="bienvenu">
             Bienvenu dans l'administration du site
         </p>
+
+        <div class="conteneur">
+            <div class="reservations">
+                <h2>Réservations</h2>
+                <p>bob</p>
+            </div>
+            <div class="statistiques">
+                <h2>Statistiques</h2>
+                <p>ette</p>
+            </div>
+        </div>
     </main>
 </body>
 </html>
