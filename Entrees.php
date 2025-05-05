08b1504c6700cb56e2f134107c47ee1475cb88d9
Tommy rabaraona-Lincourt <1524528@cstj.qc.ca>
2025-05-05 12:57:00 -0400

    Bebas (rajout du font dans tout les pages)

--- Entrees.php
+++ Entrees.php
@@ -41,6 +41,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Caveat:wght@400..700&display=swap" rel="stylesheet">
     <title>Les Rives Boréales - Entrées</title>
 </head>
 <body>
