396f76ce37a01f360807875c40b3c57ce39dfd28
Mikael <2113638@cstj.qc.ca>
2025-05-05 10:48:07 -0400

    Changement du nom de la base de donnees pour des minuscules

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
 
-$bdd = new PDO("sqlite:" . __DIR__ . "/../DB.sqlite");
+$bdd = new PDO("sqlite:" . __DIR__ . "/../db.sqlite");
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
